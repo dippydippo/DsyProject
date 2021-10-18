47481d231142a59b36af4fda87f66678e51d5b83
DippyDippo <74385136+dippydippo@users.noreply.github.com>
2021-10-18 08:56:04 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeeWoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Storage.LoadGoods();
+            Console.WriteLine(Storage.ReturnGoods());
+            while (1 == 1) Console.ReadLine();
+        }      
+    }
+}
